6dda8c3e6eca63df4385688a3a0adb08e4a1b24a
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-07-10 00:57:41 +0000

    Update from https://github.com/RiadhAdrani/slick-portfolio-svelte/commit/94cda94f575a8c422db7b918a6c9d7bdb3dfaeea

--- src/lib/params.ts
+++ src/lib/params.ts
@@ -1,15 +1,20 @@
-import { EmploymentType, Technologies, Assets, type SkillsParams } from './utils';
-import type {
-	CareerParams,
-	HomeParams,
-	NavBarParams,
-	PersonalProjectsParams,
-	ResumePageParams
-} from './utils';
+import MY_EXPERIENCES from './experiences.params';
+import MY_PROJECTS from './projects.params';
+import MY_SKILLS from './skills.params';
+import {
+	Platform,
+	type HomePageParams,
+	type ProjectPageParams,
+	type ExperiencePageParams,
+	type SkillsPageParams,
+	type ResumePageParams,
+	type SearchPageParams
+} from './types';
+import { Icons } from './utils';
 
-export const PortfolioTitle = 'Riadh Adrani';
+export const TITLE_SUFFIX = 'Riadh Adrani';
 
-export const NavBar: NavBarParams = {
+export const NavBar = {
 	home: 'Home',
 	personal: 'Projects',
 	career: 'Experiences',
@@ -17,428 +22,63 @@ export const NavBar: NavBarParams = {
 	skills: 'Skills'
 };
 
-export const Home: HomeParams = {
-	title: 'Home Page',
+export const getPlatfromIcon = (platform: Platform): Icons => {
+	switch (platform) {
+		case Platform.GitHub:
+			return Icons.GitHub;
+		case Platform.Linkedin:
+			return Icons.LinkedIn;
+		case Platform.StackOverflow:
+			return Icons.StackOverflow;
+		case Platform.Facebook:
+			return Icons.Facebook;
+		case Platform.Email:
+			return Icons.Email;
+		case Platform.Twitter:
+			return Icons.Twitter;
+		case Platform.Youtube:
+			return Icons.Youtube;
+	}
+};
+
+export const HOME: HomePageParams = {
+	title: 'Home',
 	name: 'Riadh',
 	lastName: 'Adrani',
-	description: 'A self-made developer based in Tunisia.',
-	links: {
-		github: 'https://github.com/RiadhAdrani',
-		linkedin: 'https://www.linkedin.com/in/riadh-adrani/',
-		stackoverflow: 'https://stackoverflow.com/users/12978534/riadh-adrani'
-	},
-	skills: [
-		Technologies.TypeScript,
-		Technologies.Dart,
-		Technologies.Kotlin,
-		Technologies.NodeJs,
-		Technologies.Deno,
-		Technologies.ReactJs,
-		Technologies.Svelte,
-		Technologies.VueJs,
-		Technologies.Angular,
-		Technologies.Flutter,
-		Technologies.Electron,
-		Technologies.Fastify,
-		Technologies.MongoDB,
-		Technologies.PostgreSQL,
-		Technologies.Redis
-	]
-};
-
-export const PersonalProjects: PersonalProjectsParams = {
-	title: 'Personal Projects',
-	items: [
+	description: 'A self-made software engineer',
+	links: [
+		{ platform: Platform.GitHub, link: 'https://github.com/RiadhAdrani' },
 		{
-			color: '#5cad47',
-			name: 'Dom Control',
-			logo: Assets.NodeJs,
-			description: 'Create and manipulate DOM elements.',
-			period: { from: new Date(2022, 9, 28) },
-			links: [
-				{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/dom-control-js' },
-				{ label: 'NPM', to: 'https://www.npmjs.com/package/@riadh-adrani/dom-control-js' },
-				{ label: 'Docs', to: 'https://riadhadrani.github.io/dom-control-js/' }
-			],
-			technologies: [Technologies.TypeScript, Technologies.NodeJs],
-			type: 'JavaScript library'
+			platform: Platform.Linkedin,
+			link: 'https://www.linkedin.com/in/riadh-adrani/'
 		},
 		{
-			color: '#fbb03b',
-			name: 'Werewolves',
-			logo: Assets.Unknown,
-			description:
-				'An app to manage your game of Loup garous de thiercelieu also known as The Werewolves of Millers Hollow.',
-			period: { from: new Date(2022, 6, 23) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/werewolves' }],
-			technologies: [Technologies.Dart, Technologies.Flutter],
-			type: 'Mobile App'
-		},
-		{
-			color: '#ff3e00',
-			name: 'Portfolio',
-			logo: Assets.Unknown,
-			description: 'My personal website portfolio.',
-			period: { from: new Date(2021, 3, 1) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/RiadhAdrani' }],
-			technologies: [
-				Technologies.TypeScript,
-				Technologies.NodeJs,
-				Technologies.Svelte,
-				Technologies.Sass
-			],
-			type: 'Website'
-		},
-		{
-			color: '#cf2026',
-			name: 'Recursive',
-			logo: Assets.Recursive,
-			description:
-				'Recursive is an ongoing project that allow developers to build a functional, component-based, Javascript only, beautiful and interactive Web applications.',
-			period: { from: new Date(2021, 8, 1), to: new Date(2022, 11, 26) },
-			links: [
-				{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/recursive' },
-				{ label: 'NPM', to: 'https://www.npmjs.com/package/@riadh-adrani/recursive' }
-			],
-			technologies: [Technologies.JavaScript, Technologies.NodeJs],
-			type: 'UI Framework'
-		},
-		{
-			color: '#5cad47',
-			name: 'Utility Js',
-			logo: Assets.NodeJs,
-			description: 'My JavaScript utility functions and types.',
-			period: { from: new Date(2022, 8, 1) },
-			links: [
-				{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/utility-js' },
-				{ label: 'NPM', to: 'https://www.npmjs.com/package/@riadh-adrani/utility-js' }
-			],
-			technologies: [Technologies.TypeScript, Technologies.NodeJs],
-			type: 'JavaScript library'
-		},
-		{
-			color: '#518ac8',
-			name: 'Solid GitHub',
-			logo: Assets.SolidJs,
-			description:
-				'Web app that retrieves repositories from GitHub and displaying them with the ability to be saved and persisted in the local storage.',
-			period: { from: new Date(2022, 10, 1), to: new Date(2022, 10, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/solid-github' }],
-			technologies: [Technologies.TypeScript, Technologies.NodeJs, Technologies.SolidJs],
-			type: 'Web App'
-		},
-		{
-			name: 'Trollo',
-			type: 'Web App',
-			logo: Assets.Trello,
-			color: '#065ad4',
-			period: { from: new Date(2022, 10, 1), to: new Date(2022, 10, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/trollo' }],
-			description:
-				'Trollo is a Trello clone, intended to showcase recursive and recursive-web compatibility with Typescript as of version 0.8.0.',
-			technologies: [Technologies.TypeScript, Technologies.NodeJs, Technologies.Recursive]
-		},
-		{
-			name: 'Gradio',
-			type: 'Web App',
-			logo: Assets.Gradio,
-			color: '#52bffa',
-			period: { from: new Date(2022, 9, 1), to: new Date(2022, 9, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/gradio' }],
-			description:
-				'A web application used to create and generate gradient background for css usage.',
-			technologies: [
-				Technologies.TypeScript,
-				Technologies.NodeJs,
-				Technologies.Quasar,
-				Technologies.VueJs,
-				Technologies.Sass
-			]
-		},
-		{
-			name: 'Card Baker',
-			type: 'Web App',
-			logo: Assets.CardBaker,
-			color: '#52bffa',
-			period: { from: new Date(2022, 7, 1), to: new Date(2022, 7, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/card-baker' }],
-			description: 'Create "Yu-Gi-Oh" styled game cards.',
-			technologies: [Technologies.JavaScript, Technologies.NodeJs, Technologies.Recursive]
-		},
-		{
-			name: 'Random Name Picker',
-			type: 'Android App',
-			logo: Assets.RandomNamePicker,
-			color: '#64c6c5',
-			period: { from: new Date(2022, 7, 1), to: new Date(2022, 7, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/random-name-picker' }],
-			description:
-				'An android application used to create lists for random name picking. Lists and user preferences are saved',
-			technologies: [Technologies.Android, Technologies.Kotlin]
-		},
-		{
-			name: 'WeTube',
-			type: 'Web App',
-			logo: Assets.Unknown,
-			color: '#00b0fa',
-			period: { from: new Date(2022, 6, 1), to: new Date(2022, 6, 10) },
-			links: [
-				{ label: 'GitHub (Backend)', to: 'https://github.com/RiadhAdrani/wetube-be' },
-				{ label: 'GitHub (Frontend)', to: 'https://github.com/RiadhAdrani/wetube-fe' }
-			],
-			description:
-				'Another clone added to the list of clones that I made using my own framework Recursive for the web, and as you guessed it, its a Youtube clone.',
-			technologies: [
-				Technologies.JavaScript,
-				Technologies.NodeJs,
-				Technologies.Recursive,
-				Technologies.ExpressJs,
-				Technologies.PostgreSQL
-			]
-		},
-		{
-			name: 'Audiphy',
-			type: 'Desktop App',
-			logo: Assets.Unknown,
-			color: '#3083f6',
-			period: { from: new Date(2022, 5, 1), to: new Date(2022, 6, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/audiphy' }],
-			description:
-				'A simple audio player for Windows. Audiphy is intended to be a Spotify clone, but turned out to be an audio player for Windows OS.',
-			technologies: [
-				Technologies.JavaScript,
-				Technologies.NodeJs,
-				Technologies.Recursive,
-				Technologies.Electron
-			]
-		},
-		{
-			name: 'Wroddit',
-			type: 'Web App',
-			logo: Assets.Unknown,
-			color: '#E9ECEF',
-			period: { from: new Date(2022, 4, 1), to: new Date(2022, 4, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/wroddit' }],
-			description: 'Reddit clone made using Recursive.js for front-end and Firebase as a database.',
-			technologies: [
-				Technologies.JavaScript,
-				Technologies.NodeJs,
-				Technologies.Recursive,
-				Technologies.Firebase
-			]
-		},
-		{
-			name: 'Dwidder',
-			type: 'Web App',
-			logo: Assets.Unknown,
-			color: '#1e9eae',
-			period: { from: new Date(2022, 2, 1), to: new Date(2022, 3, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/dwidde' }],
-			description:
-				'Dwidder is a twitter clone made to showcase the potential of Recursive.js. The name came originally from a tunisian meal called Dwida as I was developping the app.',
-			technologies: [
-				Technologies.JavaScript,
-				Technologies.NodeJs,
-				Technologies.Recursive,
-				Technologies.Firebase
-			]
-		},
-		{
-			name: 'Question Submission',
-			type: 'Web App',
-			logo: Assets.Unknown,
-			color: '#ffde16',
-			period: { from: new Date(2021, 6, 1), to: new Date(2021, 7, 10) },
-			links: [
-				{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/guess-app-questions-submission' }
-			],
-			description:
-				'A website that allow users to submit their questions and answers for a possible game app.',
-			technologies: [
-				Technologies.JavaScript,
-				Technologies.NodeJs,
-				Technologies.VueJs,
-				Technologies.Firebase
-			]
-		},
-		{
-			name: 'Azur City',
-			type: 'Android App',
-			logo: Assets.Unknown,
-			color: '#01acc8',
-			period: { from: new Date(2021, 1, 1), to: new Date(2021, 4, 10) },
-			links: [{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/azur_app' }],
-			description:
-				'A prototype of a commercial app made for Azur City. The App was presented as an End of studies projects.',
-			technologies: [Technologies.Android, Technologies.Kotlin]
-		},
-		{
-			name: 'Werewolves',
-			type: 'Android App',
-			logo: Assets.Unknown,
-			color: '#fbb03b',
-			period: { from: new Date(2021, 0, 1), to: new Date(2021, 5, 10) },
-			links: [
-				{ label: 'GitHub', to: 'https://github.com/RiadhAdrani/werewolves_of_the_millers_hollow' }
-			],
-			description:
-				'Creating a guide for the game "Werewolves of The Millers Hollow" originally "Les loups garous de Thierclieux".',
-			technologies: [Technologies.Android, Technologies.Kotlin]
-		},
-		{
-			name: 'Color Noter',
-			type: 'Cross platform App',
-			logo: Assets.Unknown,
-			color: '#8cacb9',
-			period: { from: new Date(2020, 8, 1), to: new Date(2020, 9, 10) },
-			links: [
-				{ label: 'GitHub (Android)', to: 'https://github.com/RiadhAdrani/color_note_plus' },
-				{ label: 'GitHub (Desktop)', to: 'https://github.com/RiadhAdrani/color_noter-desktop' },
-				{ label: 'GitHub (Web)', to: 'https://github.com/RiadhAdrani/color_noter-web' }
-			],
-			description:
-				'Color Noter is a color based note taking app that offer the user the ability to write and save his ideas and todos in an easy to use environment.',
-			technologies: [
-				Technologies.Android,
-				Technologies.Java,
-				Technologies.Electron,
-				Technologies.NodeJs,
-				Technologies.ReactJs,
-				Technologies.Recursive,
-				Technologies.Firebase
-			]
+			platform: Platform.StackOverflow,
+			link: 'hhttps://stackoverflow.com/users/12978534/riadh-adrani'
 		}
 	]
 };
 
-export const ProfessionalCareerParams: CareerParams = {
-	title: 'Professional Experience',
-	items: [
-		{
-			title: 'Fullstack Developer',
-			company: {
-				name: 'HSTECH',
-				logo: Assets.HsTech
-			},
-			description: '',
-			employmentType: EmploymentType.FullTime,
-			industry: 'Software Development',
-			location: 'Remote, Tunisia',
-			period: { from: new Date(2022, 9, 3) },
-			skills: [
-				Technologies.Deno,
-				Technologies.TypeScript,
-				Technologies.NodeJs,
-				Technologies.Svelte,
-				Technologies.Quasar,
-				Technologies.VueJs,
-				Technologies.Nuxt,
-				Technologies.ReactJs,
-				Technologies.Sass,
-				Technologies.Tailwind,
-				Technologies.Redis,
-				Technologies.MongoDB
-			]
-		},
-		{
-			title: 'Software Development Freelancer',
-			company: {
-				name: 'Self-employed',
-				logo: Assets.Unknown
-			},
-			description:
-				'Creating Mobile and Web Apps for customers using various technologies like "Xamarin", "Native Android", "React" ...',
-			employmentType: EmploymentType.Freelance,
-			industry: 'Software Development',
-			location: 'Tunisia',
-			period: { from: new Date(2021, 10, 1), to: new Date(2022, 5, 16) },
-			skills: [
-				Technologies.JavaScript,
-				Technologies.NodeJs,
-				Technologies.ReactJs,
-				Technologies.Android,
-				Technologies.Kotlin,
-				Technologies.Firebase,
-				Technologies.Xamarin,
-				Technologies.Csharp
-			]
-		},
-		{
-			title: 'Web Designer',
-			company: {
-				name: 'TriWeb',
-				logo: Assets.TriWeb
-			},
-			description:
-				'My job consisted of designing (Using Adobe Photoshop and Illustrator) the graphical charter of turnkey websites and integrating them with the help of a custom CMS made for TriWeb.',
-			employmentType: EmploymentType.FullTime,
-			industry: 'UI Design',
-			location: 'Tunis, Tunisia',
-			period: { from: new Date(2021, 4, 1), to: new Date(2021, 11, 12) },
-			skills: [
-				Technologies.HTML,
-				Technologies.CSS,
-				Technologies.JavaScript,
-				Technologies.Photoshop,
-				Technologies.Illustrator
-			]
-		},
-		{
-			title: 'Graphic Designer',
-			company: {
-				name: 'Self-employed',
-				logo: Assets.Unknown
-			},
-			description:
-				'Designing graphics for small startups and companies, Creating logos, posters ... etc',
-			employmentType: EmploymentType.Freelance,
-			industry: 'Graphic Design',
-			location: 'Tunisia',
-			period: { from: new Date(2020, 5, 1), to: new Date(2021, 11, 12) },
-			skills: [
-				Technologies.Photoshop,
-				Technologies.AfterEffects,
-				Technologies.Illustrator,
-				Technologies.Premiere
-			]
-		},
-		{
-			title: 'Intern',
-			company: {
-				name: 'Brain Academy Tunisia',
-				logo: Assets.BrainAcademy
-			},
-			description:
-				'We developed the prototype of "SMARTchair" which is an EEG controlled wheelchair. Using our skills was not enough, so it was imminent for us to learn and get used to many other programming languages, software and even new operating systems. The project was defended the 19th of July 2020 and got "Very Good" as honor.',
-			employmentType: EmploymentType.Internship,
-			industry: 'Robotics',
-			location: 'Ben Arous, Tunisia',
-			period: { from: new Date(2020, 0, 1), to: new Date(2020, 6, 19) },
-			skills: [Technologies.Python]
-		}
-	]
+export const PROJECTS: ProjectPageParams = {
+	title: 'Projects',
+	items: MY_PROJECTS
 };
 
-export const MySkillsParams: SkillsParams = {
-	skills: [
-		{
-			technology: Technologies.JavaScript,
-			description: 'I think I unlocked all Javascript and have almost 100% achievement.'
-		},
-		{
-			technology: Technologies.TypeScript,
-			description:
-				'Being a fanatic in the JavaScript land made the transition to TypeScript easier.'
-		}
-	],
-	title: 'My Skills'
+export const EXPERIENCES: ExperiencePageParams = {
+	title: 'Experiences',
+	items: MY_EXPERIENCES
 };
 
-export const ResumeParams: ResumePageParams = {
+export const SKILLS: SkillsPageParams = {
+	title: 'Skills',
+	items: MY_SKILLS
+};
+
+export const RESUME: ResumePageParams = {
 	title: 'Resumé',
-	cv: ''
+	item: ''
 };
 
-export const SearchPageParams = {
+export const SEARCH: SearchPageParams = {
 	title: 'Search'
 };
---
+++ src/lib/skills.params.ts
@@ -0,0 +1,205 @@
+import Assets from './data/assets';
+import type { Skill } from './types';
+
+const s = (skill: Skill) => skill;
+
+export type ArrayElementType<ArrayType extends readonly unknown[]> =
+	ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+const MY_SKILLS = [
+	s({
+		slug: 'js',
+		color: 'yellow',
+		description: 'I think I unlocked all Javascript and have almost 100% achievement.',
+		logo: Assets.JavaScript,
+		name: 'Javascript'
+	}),
+	s({
+		slug: 'ts',
+		color: 'blue',
+		description: 'Being a fanatic in the JavaScript land made the transition to TypeScript easier.',
+		logo: Assets.TypeScript,
+		name: 'Typescript'
+	}),
+	s({
+		slug: 'css',
+		color: 'blue',
+		description: '',
+		logo: Assets.CSS,
+		name: 'CSS'
+	}),
+	s({
+		slug: 'html',
+		color: 'orange',
+		description: '',
+		logo: Assets.HTML,
+		name: 'HTML'
+	}),
+	s({
+		slug: 'sass',
+		color: 'pink',
+		description: '',
+		logo: Assets.Sass,
+		name: 'Sass'
+	}),
+	s({
+		slug: 'reactjs',
+		color: 'cyan',
+		description: '',
+		logo: Assets.ReactJs,
+		name: 'React Js'
+	}),
+	s({
+		slug: 'svelte',
+		color: 'orange',
+		description: '',
+		logo: Assets.Svelte,
+		name: 'Svelte'
+	}),
+	s({ slug: 'vue', color: 'green', description: '', logo: Assets.VueJs, name: 'VueJs' }),
+	s({ slug: 'nuxt', color: 'green', description: '', logo: Assets.Nuxt, name: 'Nuxt' }),
+	s({ slug: 'node', color: 'green', description: '', logo: Assets.NodeJs, name: 'Node JS' }),
+	s({
+		slug: 'deno',
+		color: 'white',
+		description: '',
+		logo: Assets.Deno,
+		name: 'Deno'
+	}),
+	s({ slug: 'dart', color: 'cyan', description: '', logo: Assets.Dart, name: 'Dart' }),
+	s({ slug: 'flutter', color: 'green', description: '', logo: Assets.Flutter, name: 'Flutter' }),
+	s({ slug: 'solid', color: 'blue', description: '', logo: Assets.SolidJs, name: 'Solid JS' }),
+	s({ slug: 'quasar', color: 'purple', description: '', logo: Assets.Quasar, name: 'Quasar' }),
+	s({ slug: 'kotlin', color: 'purple', description: '', logo: Assets.Kotlin, name: 'Kotlin' }),
+	s({
+		slug: 'express',
+		color: 'white',
+		description: '',
+		logo: Assets.ExpressJs,
+		name: 'Express JS'
+	}),
+	s({
+		slug: 'electron',
+		color: 'purple',
+		description: '',
+		logo: Assets.Electron,
+		name: 'Electron JS'
+	}),
+	s({
+		slug: 'postgresql',
+		color: 'blue',
+		description: '',
+		logo: Assets.PostgreSQL,
+		name: 'PostgreSQL'
+	}),
+	s({
+		slug: 'firebase',
+		color: 'orange',
+		description: '',
+		logo: Assets.Firebase,
+		name: 'Firebase'
+	}),
+	s({
+		slug: 'mongodb',
+		color: 'green',
+		description: '',
+		logo: Assets.MongoDB,
+		name: 'MongoDB'
+	}),
+	s({
+		slug: 'java',
+		color: 'red',
+		description: '',
+		logo: Assets.Java,
+		name: 'Java'
+	}),
+	s({
+		slug: 'angular',
+		color: 'red',
+		description: '',
+		logo: Assets.Angular,
+		name: 'Angular'
+	}),
+	s({
+		slug: 'redis',
+		color: 'red',
+		description: '',
+		logo: Assets.Redis,
+		name: 'Redis'
+	}),
+	s({
+		slug: 'tailwind',
+		color: 'cyan',
+		description: '',
+		logo: Assets.Tailwind,
+		name: 'Tailwind'
+	}),
+	s({
+		slug: 'csharp',
+		color: 'purple',
+		description: '',
+		logo: Assets.Csharp,
+		name: 'C#'
+	}),
+	s({
+		slug: 'xamarin',
+		color: 'purple',
+		description: '',
+		logo: Assets.Xamarin,
+		name: 'Xamarin'
+	}),
+	s({
+		slug: 'python',
+		color: 'yellow',
+		description: '',
+		logo: Assets.Python,
+		name: 'Python'
+	}),
+	s({
+		slug: 'ps',
+		color: 'cyan',
+		description: '',
+		logo: Assets.Photoshop,
+		name: 'Adobe Photoshop'
+	}),
+	s({
+		slug: 'ae',
+		color: 'purple',
+		description: '',
+		logo: Assets.AfterEffects,
+		name: 'Adobe After Effects'
+	}),
+	s({
+		slug: 'pp',
+		color: 'pink',
+		description: '',
+		logo: Assets.Premiere,
+		name: 'Adobe Premiere'
+	}),
+	s({
+		slug: 'ai',
+		color: 'orange',
+		description: '',
+		logo: Assets.Illustrator,
+		name: 'Adobe Illustrator'
+	}),
+	s({
+		slug: 'docker',
+		color: 'blue',
+		description: '',
+		logo: Assets.Docker,
+		name: 'Docker'
+	}),
+	s({
+		slug: 'fastify',
+		color: 'fastify',
+		description: '',
+		logo: Assets.Fastify,
+		name: 'Fastify'
+	})
+];
+
+export default MY_SKILLS;
+
+export const getSkills = (...slugs: Array<string>): Array<Skill> =>
+	MY_SKILLS.filter((it) => slugs.includes(it.slug));
