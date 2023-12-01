2b5bc865600d344a2c623ae33d8ee3b9393bcb8c
RiadhAdrani <riadh_adrani@hotmail.fr>
2023-12-01 23:07:40 +0000

    Update from https://github.com/RiadhAdrani/slick-portfolio-svelte/commit/a628f9453be15e059f4a507fa4f255b0a59b1b07

--- src/lib/data/assets.ts
+++ src/lib/data/assets.ts
@@ -1,9 +1,9 @@
 import type { Asset } from '$lib/types';
 import { theme } from '$lib/stores/theme';
+import { base } from '$app/paths';
 import Azur from '$lib/assets/logos/azur.png';
 
-const gh = (file: string) =>
-	`https://raw.githubusercontent.com/RiadhAdrani/slick-portfolio-svelte/assets/logos/${file}`;
+const gh = (file: string) => `${base}/logos/${file}`;
 
 const a = (light: string, dark?: string): Asset =>
 	dark ? { dark: gh(dark), light: gh(light) } : gh(light);
@@ -14,7 +14,7 @@ const Assets = {
 	C: a('c.svg'),
 	Cpp: a('cpp.svg'),
 	Celery: a('celery.svg'),
-	Django: a('djanog.svg'),
+	Django: a('django.svg'),
 	FastApi: a('fastapi'),
 	Flask: a('flask.svg'),
 	Go: a('go.svg'),
