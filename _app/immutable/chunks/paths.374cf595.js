83241c0b543006d4ebe0b8a82a10cbd533cb8d38
RiadhAdrani <54232641+RiadhAdrani@users.noreply.github.com>
2024-01-01 11:21:16 +0000

    Deploying to gh-pages from @ RiadhAdrani/RiadhAdrani@1041daf238c35a117e2a3d86adbf7117ed6d5313 🚀

--- _app/immutable/chunks/paths.374cf595.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_19e7m0v)==null?void 0:s.base)??"/RiadhAdrani";var a;const t=((a=globalThis.__sveltekit_19e7m0v)==null?void 0:a.assets)??e;export{t as a,e as b};
