d3cbd9c5eb9ffcc787e014442764b9c31338740d
RiadhAdrani <54232641+RiadhAdrani@users.noreply.github.com>
2024-08-05 19:08:28 +0000

    Deploying to gh-pages from @ RiadhAdrani/RiadhAdrani@77ae0d82b72ea78ecf16284fcaa29242f3fd76f9 🚀

---
+++ _app/immutable/chunks/stores.f903e287.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.397f9daa.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
